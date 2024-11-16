890e0c43b00849be29fece2e44d30597316502f9
patil-roshan <roshasnpatil@nimapinfotech.com>
2024-11-16 22:07:08 +0530

    Project init and added shadcm componments with dark & light  theme

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import { ModeToggle } from '@/components/ui/theme-toggle'
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Dashboard
